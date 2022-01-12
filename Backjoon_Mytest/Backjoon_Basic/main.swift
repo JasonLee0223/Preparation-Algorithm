758e41a3d2a84ee7b7e0ae5817090cce990710ef
Jason <ggj00125@gmail.com>
2022-01-12 14:24:05 +0900

    Upload file

---
+++ Backjoon_Mytest/Backjoon_Basic/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Backjoon_Basic
+//
+//  Created by 이건행 on 2021/12/13.
+//
+
+import Foundation
+
+//num4673()
+//print(num1065())
+
+//MARK: CS1 - 알고리즘
+//num1000() // 1 2 -> 3
+//num1009()
+num1076()
---
+++ Leetcode_Mytest/Leetcode_Basic/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Leetcode_Basic
+//
+//  Created by 이건행 on 2022/01/12.
+//
+
+import Foundation
+
+print("Hello, World!")
+
